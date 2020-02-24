fe2b2401146e8f7bfcc08d0de7e6b2811deeac3b
iswa25 <wala.benbrahim@esprit.tn>
2020-02-24 16:16:54 +0100

    Add a new module for client

---
+++ clientui/src/main/java/com/clientui/controller/ClientController.java
@@ -0,0 +1,17 @@
+package com.clientui.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ClientController {
+
+    @RequestMapping("/")
+    public String acceuil(Model model){
+
+        return "Acceuil";
+
+    }
+}

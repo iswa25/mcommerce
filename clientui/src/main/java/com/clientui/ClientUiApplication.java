acb2f1151ad3e64f4c9254a61ab7d824cb1e7004
iswa25 <wala.benbrahim@esprit.tn>
2020-02-26 11:49:23 +0100

    Faire communiquer les MS grace à Feign

--- clientui/src/main/java/com/clientui/ClientUiApplication.java
+++ clientui/src/main/java/com/clientui/ClientUiApplication.java
@@ -2,12 +2,14 @@ package com.clientui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-public class ClientUiApplication {
+@EnableFeignClients("com.clientui")
+public class ClientuiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ClientUiApplication.class, args);
+		SpringApplication.run(ClientuiApplication.class, args);
 	}
 
 }

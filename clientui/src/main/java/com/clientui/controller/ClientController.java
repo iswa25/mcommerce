405e2e8052a5f2ad61763adc63e8e27279a123a9
iswa25 <wala.benbrahim@esprit.tn>
2020-02-26 13:34:56 +0100

    Faire communiquer les MS grace à Feign part 2

--- clientui/src/main/java/com/clientui/controller/ClientController.java
+++ clientui/src/main/java/com/clientui/controller/ClientController.java
@@ -5,6 +5,7 @@ import com.clientui.proxies.MicroserviceProduitsProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -16,7 +17,7 @@ public class ClientController {
    private  MicroserviceProduitsProxy mProduitsProxy;
 
     @RequestMapping("/")
-    public String acceuil(Model model){
+    public String accueil(Model model){
 
        List<ProductBean> produits = mProduitsProxy.listeDesProduits();
 
@@ -24,6 +25,16 @@ public class ClientController {
 
         return "Accueil";
 
+    }
+
+    @RequestMapping("/details-produit/{id}")
+    public String ficheProduit(@PathVariable int id, Model model){
+
+        ProductBean produit = mProduitsProxy.recupererUnProduit(id);
+
+        model.addAttribute("produit", produit);
+
+        return "FicheProduit";
     }
 
 }
